097f0ade8b50431b4ea7f4c9a04bcddf6b5d6039
Bryce Eadie <bryce.eadie@gmail.com>
2018-12-13 08:55:46 +0000

    Automatically backed up by Learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,11 @@
 module SongsHelper
 
+  def display_artist(artist)
+    if artist.name != ""
+      link_to artist_path, artist
+    else
+      link_to "Add Artist", edit_artist_path(artist)
+    end
+  end
+
 end
